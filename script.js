223f3181f63ec71e07b10ea0d77edae9d28d5cbd
Yousef ayman <yousefayman2000500@gmail.com>
2024-08-01 12:28:07 +0300

    updating the UI according to the user input

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-const apiKey ="7a790157fdc0f0770cb4dfd11af5f843";
-const city = "cairo";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+const apiKey = "7a790157fdc0f0770cb4dfd11af5f843";
+let city = "paris";
+const searchButton = document.querySelector('button');
+const cityInputField =document.querySelector('.city-name')
 
-fetch(apiUrl).then(res=>{
-    if(!res.ok){
-        console.error(`HTTP error, Statues: ${res.status}`)
-    }return res.json();
-}).then(data=>console.log(data)).catch(err=>console.error(`there was a problem with fetching data ${err}`))
+const updateCity = () => {
+    const cityInput = cityInputField.value;
+    city = cityInput;
+    updateWeatherForCity();
+};
+
+const updateWeatherForCity = () => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    
+    fetch(apiUrl).then(res => {
+        if (!res.ok) {
+            console.error(`HTTP error, Status: ${res.status}`);
+        }
+        return res.json();
+    }).then(data => {
+        updateWeather(data);
+    }).catch(err => console.error(`There was a problem with fetching data ${err}`));
+};
+
+const updateWeather = (data) => {
+    document.querySelector(".city").textContent = `${data['name']}`;
+    document.querySelector(".temp").textContent = `${(data.main.temp - 273.15).toFixed(0)}°C`;
+    document.querySelector(".humidity-measure").textContent = `${data.main.humidity}%`;
+    document.querySelector(".wind-measure").textContent = `${data.wind.speed}km/h`;
+    document.querySelector(".description").textContent = `${data.weather[0].description.toUpperCase()}`;   
+};
+
+searchButton.addEventListener('click', updateCity);
+
+cityInputField.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        updateCity();
+    }
+});
+
+
+
+updateWeatherForCity();
